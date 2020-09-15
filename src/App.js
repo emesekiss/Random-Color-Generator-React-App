03a5609d3ef06818a5fad9b56082dad1f35fe032
Emese Kiss <emesekiss92@yahoo.com>
2020-09-15 16:29:37 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+//This is a function component:
+
+const App = () => {
+  //Use hooks here before the return statement:
+
+  const [color, setColor] = useState('#ffffff');
+  const randomHex = () => {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+    setColor(randomColor);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App" style={{ backgroundColor: `${color}` }}>
+        <header>Welcome to Random Color Generator!</header>
+        <h1>{color}</h1>
+        <button onClick={randomHex}>Generate Color</button>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
