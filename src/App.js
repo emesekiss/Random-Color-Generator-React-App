35cc1037cab8926192e2076dd50d27b6535c9718
Emese Kiss <emesekiss92@yahoo.com>
2020-09-16 17:33:22 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React, { useState } from 'react';
+import Header from './Header';
+import './Style.css';
 
 //This is a function component:
 
 const App = () => {
   //Use hooks here before the return statement:
 
-  const [color, setColor] = useState('#ffffff');
+  const [color, setColor] = useState('');
   const randomHex = () => {
     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 
     setColor(randomColor);
   };
+
   return (
-    <>
-      <div className="App" style={{ backgroundColor: `${color}` }}>
-        <header>Welcome to Random Color Generator!</header>
-        <h1>{color}</h1>
+    <React.Fragment>
+      <Header />
+      <div className="colorthis">
+        <div style={{ backgroundColor: `${color}` }} className="hex">
+          Hex code: {color}
+        </div>
         <button onClick={randomHex}>Generate Color</button>
       </div>
-    </>
+    </React.Fragment>
   );
 };
 
---
+++ src/Header.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Style.css';
+
+const Header = () => <header>Welcome to Random Color Generator!</header>;
+
+export default Header;
